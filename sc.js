b5b0dc303f783a71cf958ac89106f86bc6d3de1b
harshitchauhan544 <harshitchauhan544@gmail.com>
2025-03-29 12:18:58 +0530

    Leetcode part Complete

--- sc.js
+++ sc.js
@@ -8,9 +8,9 @@ document.addEventListener("DOMContentLoaded",function(){
     const mediumProgress = document.querySelector(".medium-progress");
     const hardProgress = document.querySelector(".hard-progress");
 
-    const easyLabel = document.getElementById('.easy-label');
-    const mediumLabel = document.getElementById('.medium-label');
-    const hardLabel = document.getElementById('.hard-label');
+    const easyLabel = document.getElementById('easy-label');
+    const mediumLabel = document.getElementById('medium-label');
+    const hardLabel = document.getElementById('hard-label');
 
     const statsCardContainer = document.querySelector('.stats-card'); 
 
@@ -29,12 +29,68 @@ document.addEventListener("DOMContentLoaded",function(){
         return isMatching;
     }
 
+    const updateProgress = (solved,total,label,circle) => {
+        console.log(solved,total);
+        const progressDegree = Math.ceil(Number((solved/total)*100));
+        label.textContent = `${solved}/${total}`
+        circle.style.setProperty("--progress-degree",`${progressDegree}%`);
+    }
+
+    const showData = (data)=> {
+        // console.log(data);
+        // const t_qs = data.totalQuestions;
+        const t_easy_qs = data.totalEasy;
+        const t_medium_qs = data.totalMedium;
+        const t_hard_qs = data.totalHard;
+
+        // const s_qs = data.totalSolved;
+        const s_easy_qs = data.easySolved;
+        const s_medium_qs = data.mediumSolved;
+        const s_hard_qs = data.hardSolved;
+        // console.log(t_hard_qs)
+
+        updateProgress(s_easy_qs,t_easy_qs,easyLabel,easyProgress);
+        updateProgress(s_medium_qs,t_medium_qs,mediumLabel,mediumProgress);
+        updateProgress(s_hard_qs,t_hard_qs,hardLabel,hardProgress);
+
+        const cardData = [
+            {
+                label : "Total Solved",
+                value : data.totalSolved
+            },
+            {
+                label : "Accepatnce rate",
+                value : data.acceptanceRate
+            },
+            {
+                label : "Ranking",
+                value : data.ranking
+            }
+
+        ]
+
+        statsCardContainer.innerHTML = cardData.map(
+            (data) => {
+                return `
+                    <div class = 'card'>
+                        <h3>${data.label}</h3>
+                        <p>${data.value}</p>
+                    </div>
+                `
+            }
+        ).join("")
+
+        
+
+    }
+
     async function fetchUserDetails(username) {
         const url = `https://leetcode-stats-api.herokuapp.com/${username}`
         try{
 
             searchButton.textContent = "Searching..";
             searchButton.disabled = true;
+            statsCardContainer.style.display = 'none';
 
             const response = await fetch(url);
             if(!response.ok){
@@ -42,18 +98,21 @@ document.addEventListener("DOMContentLoaded",function(){
             }
 
             const data = await response.json();
-            console.log(data);
+            
+            showData(data);
 
             // displayUserData(data);
             
         }
         catch(error){
+            console.log(error);
             statsContainer.innerHTML = `<p>No Data Found</p>`;
 
         }
         finally{
             searchButton.textContent = "Search";
             searchButton.disabled = false;
+            statsCardContainer.style.display = 'block';
         }
 
     }
